63cd410adc1723244524a59bf6fc2a607db2aa1d
Stetson Harding <stetson2001@msn.com>
2021-09-14 06:21:17 -0700

    set up deposit and withdraw dispatch, made UI.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./redux/index";
 
 function App() {
-  const state = useSelector((state) => state.account);
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
 
-  console.log(state);
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1000)}> Deposit </button>
+      <button onClick={() => withdrawMoney(1000)}> Withdraw </button>
+    </div>
+  );
 }
 
 export default App;
